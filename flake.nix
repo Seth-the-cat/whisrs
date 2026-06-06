d028d3640b124c9b38a0aea40463e5061e564d13
Seth the cat <71609066+Seth-the-cat@users.noreply.github.com>
2026-06-05 19:48:18 -0500

    allow the evil
    Import nixpkgs with custom configuration for unfree packages and CUDA support.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,13 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+        };
 
         nativeBuildInputs = with pkgs; [
           pkg-config
